10c0ab01b5680e7b33e865a07f0aba0bb6e9bd90
Jason Frame <jason@onehackoranother.com>
2014-11-20 15:49:56 +0000

    Parser test

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var fs          = require('fs');
 var filename    = process.argv[2];
 var source      = fs.readFileSync(filename, 'utf8');
 
-// var parser = require('./lib/parser');
-// var ast = parser.parse(source + "\n");
+var parser = require('./lib/parser');
+var ast = parser.parse(source + "\n");
 
-// console.log(require('util').inspect(ast, {depth: null, colors: true}));
+console.log(require('util').inspect(ast, {depth: null, colors: true}));
 
 // require('./lib/interpret')(ast);
